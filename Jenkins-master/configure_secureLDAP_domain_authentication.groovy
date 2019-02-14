dbeb6577ae588780a9b6d3b69fa90d6e6b3fd885
Purvis <ian.purvis@fujitsu.com>
2019-02-14 14:10:25 +0000

    rebase

---
+++ Jenkins-master/configure_secureLDAP_domain_authentication.groovy
@@ -0,0 +1,38 @@
+import java.lang.System
+import jenkins.model.*
+import hudson.plugins.active_directory.*
+import com.google.common.collect.Lists
+
+
+
+def domain = "iansdomain.com"
+def site = null
+def bindName = null
+def bindPassword = null
+def server = "win2016dc-2.iansdomain.com:389"
+def groupLookupStrategy = "RECURSIVE"
+def tlsConfiguration = "JDK_TRUSTSTORE"
+
+def instance = Jenkins.getInstance()
+
+
+println "--> Configure AD"
+ActiveDirectorySecurityRealm realm = new ActiveDirectorySecurityRealm(domain,
+                                                                      Lists.newArrayList(new ActiveDirectoryDomain(domain, server)),
+                                                                      site,
+                                                                      bindName,
+                                                                      bindPassword,
+                                                                      server,
+                                                                      GroupLookupStrategy.valueOf(groupLookupStrategy.toString().toUpperCase()),
+							                                          false,									                                    false,
+									                                  null,
+									                                  false
+                                                                      )
+                    
+realm.getDomains().each({
+  it.bindName = realm.bindName
+  it.bindPassword = realm.bindPassword
+})
+  
+Jenkins.instance.setSecurityRealm(realm)
+Jenkins.instance.save()
